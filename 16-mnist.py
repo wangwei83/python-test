1a5dfe3bf6c2a13e84703d70fc7aadb649c78df6
wangwei83 <wangwei83@cuit.edu.cn>
2024-10-06 00:01:38 +0800

    mnist keras   train

--- 16-mnist.py
+++ 16-mnist.py
@@ -1,26 +1,95 @@
-#
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
+# import tensorflow as tf
+# import tensorflow_datasets as tfds
+# import matplotlib.pyplot as plt
+
+# # 加载 MNIST 数据集
+# (train_data, test_data), info = tfds.load('mnist', split=['train', 'test'], with_info=True, as_supervised=True)
+
+# # 预处理数据
+# def preprocess(image, label):
+#     image = tf.cast(image, tf.float32) / 255.0
+#     label = tf.one_hot(label, depth=10)
+#     return image, label
+
+# train_data = train_data.map(preprocess).batch(32)
+# test_data = test_data.map(preprocess).batch(32)
+
+# # 构建一个简单的模型
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Flatten(input_shape=(28, 28)),
+#     tf.keras.layers.Dense(128, activation='relu'),
+#     tf.keras.layers.Dropout(0.2),
+#     tf.keras.layers.Dense(10, activation='softmax')
+# ])
+
+# # 编译模型
+# model.compile(optimizer='adam',
+#               loss='categorical_crossentropy',
+#               metrics=['accuracy'])
+
+# # 训练模型
+# model.fit(train_data, epochs=5)
+
+# # 评估模型
+# model.evaluate(test_data)
+
+# # 可视化样本
+# for image, label in train_data.take(1):
+#     plt.imshow(image[0].numpy().reshape(28, 28), cmap='gray')
+#     plt.title(f'Label: {tf.argmax(label[0]).numpy()}')
+#     plt.show()
+
+
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-import matplotlib.pyplot as plt
-# MNIST_data指的是存放数据的文件夹路径，one_hot=True 为采用one_hot的编码方式编码标签
-mnist = input_data.read_data_sets('../datasets/MNIST_data/', one_hot=True)
-# load data
-train_X = mnist.train.images
-train_Y = mnist.train.labels
-print(train_X.shape, train_Y.shape)   # 输出训练集样本和标签的大小
-# 查看数据，例如训练集中第一个样本的内容和标签
-print(train_X[0])       # 是一个包含784个元素且值在[0,1]之间的向量
-print(train_Y[0])
-# 可视化样本，下面是输出了训练集中前4个样本
-fig, ax = plt.subplots(nrows=2, ncols=2, sharex='all', sharey='all')
-ax = ax.flatten()
-for i in range(4):
-    img = train_X[i].reshape(28, 28)
-    # ax[i].imshow(img,cmap='Greys')
-    ax[i].imshow(img)
-ax[0].set_xticks([])
-ax[0].set_yticks([])
-plt.tight_layout()
-plt.show()
+import numpy as np
+import gzip
+
+def load_mnist_images(filename):
+    with gzip.open(filename, 'rb') as f:
+        # Skip the magic number and dimensions
+        f.read(16)
+        buffer = f.read()
+        data = np.frombuffer(buffer, dtype=np.uint8).astype(np.float32)
+        data = data.reshape(-1, 28, 28, 1)
+        data = data / 255.0  # Normalize to [0, 1]
+        return data
+
+def load_mnist_labels(filename):
+    with gzip.open(filename, 'rb') as f:
+        # Skip the magic number and dimensions
+        f.read(8)
+        buffer = f.read()
+        labels = np.frombuffer(buffer, dtype=np.uint8)
+        return labels
+
+# 加载训练和测试数据  /home/wangwei83/Desktop/python-test/dataset/mnist/t10k-labels-idx1-ubyte.gz
+train_images = load_mnist_images('/home/wangwei83/Desktop/python-test/dataset/mnist/train-images-idx3-ubyte.gz')
+train_labels = load_mnist_labels('/home/wangwei83/Desktop/python-test/dataset/mnist/train-labels-idx1-ubyte.gz')
+test_images = load_mnist_images('/home/wangwei83/Desktop/python-test/dataset/mnist/t10k-images-idx3-ubyte.gz')
+test_labels = load_mnist_labels('/home/wangwei83/Desktop/python-test/dataset/mnist/t10k-labels-idx1-ubyte.gz')
+
+print(train_images.shape)
+# 将标签转换为 one-hot 编码
+train_labels = tf.keras.utils.to_categorical(train_labels, 10)
+test_labels = tf.keras.utils.to_categorical(test_labels, 10)
+
+# 构建一个简单的模型
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# 编译模型
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# 训练模型
+model.fit(train_images, train_labels, epochs=5, batch_size=32)
+
+# 评估模型
+model.evaluate(test_images, test_labels)
+
+print("Done!")
