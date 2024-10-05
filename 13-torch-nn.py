9e226e44d225f351d12e140684776a5396da313b
wangwei83 <wangwei83@cuit.edu.cn>
2024-10-05 08:40:11 +0800

    torch-nn

---
+++ 13-torch-nn.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+
+class SimpleNN(nn.Module):
+    def __init__(self):
+        super(SimpleNN, self).__init__()
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.softmax(x)
+        return x
+
+model = SimpleNN()
+print(model)
