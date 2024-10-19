0b98397692e768dc1f7bef482a0af10f9febbae8
wangwei83 <wangwei83@cuit.edu.cn>
2024-10-19 16:51:11 +0800

    diffusers code submitted

--- 42-diffusion.py
+++ 42-diffusion.py
@@ -1,3 +1,8 @@
-from diffusers import StableDiffusionPipeline
+# conda install -c conda-forge diffusers
 
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
+from diffusers import DiffusionPipeline
+import torch
+
+pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+pipeline.to("cuda")
+pipeline("An image of a squirrel in Picasso style").images[0]
