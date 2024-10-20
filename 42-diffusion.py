bae0b9c2197f9459aa33a207060e5d3113d7d3cb
wangwei83 <wangwei83@cuit.edu.cn>
2024-10-20 09:13:04 +0800

    diffuse huggingface

--- 42-diffusion.py
+++ 42-diffusion.py
@@ -1,8 +1,27 @@
-# conda install -c conda-forge diffusers
+# from transformers import AutoImageProcessor, ResNetForImageClassification
+# import torch
+# from datasets import load_dataset
 
-from diffusers import DiffusionPipeline
-import torch
+# dataset = load_dataset("huggingface/cats-image")
+# image = dataset["test"]["image"][0]
 
-pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
-pipeline.to("cuda")
-pipeline("An image of a squirrel in Picasso style").images[0]
+# processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
+# model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
+
+# inputs = processor(image, return_tensors="pt")
+
+# with torch.no_grad():
+#     logits = model(**inputs).logits
+
+# # model predicts one of the 1000 ImageNet classes
+# predicted_label = logits.argmax(-1).item()
+# print(model.config.id2label[predicted_label])
+
+from transformers import BertTokenizer, BertModel
+
+# 下载并加载预训练的 BERT 模型和词汇表
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+
+# 输出模型结构
+print(model)
