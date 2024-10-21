533f5c0eb6ae4c16102a51da9708d4c4cf36a632
wangwei83 <wangwei83@cuit.edu.cn>
2024-10-21 10:29:35 +0800

    update

--- 42-diffusion.py
+++ 42-diffusion.py
@@ -17,11 +17,40 @@
 # predicted_label = logits.argmax(-1).item()
 # print(model.config.id2label[predicted_label])
 
-from transformers import BertTokenizer, BertModel
+# from transformers import BertTokenizer, BertModel
 
-# 下载并加载预训练的 BERT 模型和词汇表
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertModel.from_pretrained('bert-base-uncased')
+# # 下载并加载预训练的 BERT 模型和词汇表
+# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+# # model = BertModel.from_pretrained('bert-base-uncased')
 
-# 输出模型结构
-print(model)
+# # # 输出模型结构
+# # print(model)
+
+
+from transformers import DetrImageProcessor, DetrForObjectDetection
+import torch
+from PIL import Image
+import requests
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+# you can specify the revision tag if you don't want the timm dependency
+processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+
+inputs = processor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+
+# convert outputs (bounding boxes and class logits) to COCO API
+# let's only keep detections with score > 0.9
+target_sizes = torch.tensor([image.size[::-1]])
+results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+    box = [round(i, 2) for i in box.tolist()]
+    print(
+            f"Detected {model.config.id2label[label.item()]} with confidence "
+            f"{round(score.item(), 3)} at location {box}"
+    )
+
